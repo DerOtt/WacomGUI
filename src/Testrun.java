8cf258a4c5af8b0946bfbb09dd1764d4a3092fc1
DerOtt <4050793+DerOtt@users.noreply.github.com>
2017-12-28 14:57:13 +0100

    Test successfull. We can open stuff using java

---
+++ src/Testrun.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Testrun {
+
+	public static void main(String[] args) throws IOException {
+		Runtime.getRuntime().exec("xdg-open /home/leo/TabletProperties.sh");
+		
+
+	}
+
+}
