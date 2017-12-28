3d952318c7c38f5b8bcbaa4cda125ce7d3ef5390
DerOtt <4050793+DerOtt@users.noreply.github.com>
2017-12-28 17:06:56 +0100

    I need a book
    Seriously

--- src/Testrun.java
+++ src/Testrun.java
@@ -2,15 +2,22 @@ import java.io.IOException;
 
 public class Testrun {
 
-	public static void main(String[] args) throws IOException {
-		
-		
-		
-		
-		
-		Runtime.getRuntime().exec("xdg-open /home/leo/TabletProperties.sh");
+	public static void main(String[] args) {
 		
 
+
+	}
+	
+	void runInShell(String command) throws IOException  {
+		
+	Runtime.getRuntime().exec(command);
+	
+	}
+	
+	String runInShellIO(String command) throws IOException {
+		Runtime.getRuntime().exec(command);
+		return 
+		
 	}
 
 }
